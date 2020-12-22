a42ee249c94380277efde808168dba69d20ebfdf
Ion Petropoulos <ionpetro@gmail.com>
2020-12-22 17:54:33 +0200

    Add more test cases on Input component
    Test showPassword method

--- src/components/Input/__tests__/input.spec.js
+++ src/components/Input/__tests__/input.spec.js
@@ -1,13 +1,14 @@
-import {mount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
 import Input from '../Input.vue';
 
 describe('Input', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(Input, {
+    wrapper = shallowMount(Input, {
       propsData: {
-        type: 'text'
+        type: 'text',
+        name: 'password'
       }
     });
   });
@@ -15,6 +16,48 @@ describe('Input', () => {
   describe('when loaded', () => {
     it('renders correctly', () => {
       expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('shows password on icon click', async () => {
+      wrapper = shallowMount(Input, {
+        propsData: {
+          type: 'password',
+          name: 'password'
+        }
+      });
+      expect(wrapper.vm.$data.customType).toEqual('password');
+      expect(wrapper.vm.$data.showEye).toBeTruthy();
+
+      await wrapper.find('.input__icon').trigger('click');
+      expect(wrapper.vm.$data.customType).toEqual('text');
+      expect(wrapper.vm.$data.showEye).toBeFalsy();
+    });
+
+    it('hides password on icon click', async () => {
+      wrapper = shallowMount(Input, {
+        propsData: {
+          type: 'password',
+          name: 'password'
+        }
+      });
+
+      // turn password field to text
+      wrapper.vm.$data.customType = 'text';
+      wrapper.vm.$data.showEye = false;
+
+      await wrapper.find('.input__icon').trigger('click');
+      expect(wrapper.vm.$data.customType).toEqual('password');
+      expect(wrapper.vm.$data.showEye).toBeTruthy();
+    });
+    it('does not change input type when its text input', () => {
+      wrapper = shallowMount(Input, {
+        propsData: {
+          type: 'text',
+          name: 'text'
+        }
+      });
+      wrapper.vm.showPassword();
+      expect(wrapper.vm.$data.customType).toEqual('text');
     });
   });
 });
