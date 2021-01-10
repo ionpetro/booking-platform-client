e18dc4cf2a77b0d688e53d7f917a3e868f78cefb
Ion Petropoulos <ionpetro@gmail.com>
2021-01-10 15:07:43 +0200

    Add footer component to App
    Include tests on App that check Navigation and Footer visibility across
    the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import Navigation from './components/Navigation/Navigation.vue';
+import Footer from './components/Footer/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    Navigation
+    Navigation,
+    Footer
   },
   data() {
     return {
-      disabledNavigationRoutes: ['login']
+      disabledNavigationRoutes: ['login'],
+      disabledFooterRoutes: ['login', 'bookRef']
     };
   },
   methods: {
     hideNavigation(routeName) {
       return this.disabledNavigationRoutes.includes(routeName);
+    },
+    hideFooter(routeName) {
+      return this.disabledFooterRoutes.includes(routeName);
     }
   }
 };
