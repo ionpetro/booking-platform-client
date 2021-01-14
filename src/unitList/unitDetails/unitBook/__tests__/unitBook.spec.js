8e5d3e818f1a65fbf3649924370513fc5b7523a3
Ion Petropoulos <ionpetro@gmail.com>
2021-01-14 19:53:04 +0200

    Fix test error on UnitBook

--- src/unitList/unitDetails/unitBook/__tests__/unitBook.spec.js
+++ src/unitList/unitDetails/unitBook/__tests__/unitBook.spec.js
@@ -11,10 +11,8 @@ const mockData = {
   bookDate: '1/1/2020'
 };
 const mockResp = mockData;
-
 describe('UnitBook', () => {
   const $router = {push: jest.fn()};
-
   let wrapper;
   beforeEach(() => {
     wrapper = shallowMount(UnitBook, {
@@ -22,43 +20,29 @@ describe('UnitBook', () => {
       mocks: {$router}
     });
   });
-
   afterEach(() => {
     wrapper.destroy();
   });
-
   describe('when loaded', () => {
     it('renders data correctly', async () => {
       expect(wrapper.element).toMatchSnapshot();
     });
   });
-
   describe('when book button is clicked', () => {
     it('makes the booking', async () => {
-      // set year to 2081
       wrapper.vm.yearSelected(2081);
-
-      // mock response
       UnitService.prototype.bookUnitById = jest.fn(() => Promise.resolve(mockResp));
-
       await wrapper.findComponent(Button).trigger('click');
       await flushPromises();
-
-      // expect(mockMethod).toHaveBeenCalled();
       expect($router.push).toHaveBeenCalledWith({name: 'bookRef', params: {id: 2}});
     });
-
     it('throws an error message if booking was failed', async () => {
-      // set year to 2081
       wrapper.vm.yearSelected(2081);
-
       const err = new Error('error');
-      // mock response
       UnitService.prototype.bookUnitById = jest.fn(() => Promise.reject(err));
-
       await wrapper.findComponent(Button).trigger('click');
-
-      // expect(mockMethod).toHaveBeenCalled();
+      await flushPromises();
+      expect(UnitService.prototype.bookUnitById).toHaveBeenCalled();
       expect(wrapper.emitted().onErrorMessage[0]).toEqual([err]);
     });
   });
